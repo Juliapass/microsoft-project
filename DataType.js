9f75582e43614c885314acbc8965e4dac783e846
Juliapass <127858317+Juliapass@users.noreply.github.com>
2023-03-21 13:41:57 +0100

    Add variables for measurements

--- DataType.js
+++ DataType.js
@@ -1,2 +1,6 @@
 const greetings = "Hello, nice to see you, please put your data in system";
 console.log(greetings);
+
+let measurementsOne = "chest";
+let measurementsTwo = "measure around your hip bone";
+let measurementsThree = "measure around your seat";
