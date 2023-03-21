e554cb77c0317e1edde8d2aec51ddb14e1fa94bd
Juliapass <127858317+Juliapass@users.noreply.github.com>
2023-03-21 14:00:02 +0100

    added variables for fabric type and fashion

--- DataType.js
+++ DataType.js
@@ -7,3 +7,6 @@ let measurementsThree = "measure around your seat";
 
 let dressLength = "knee-length";
 let dressSleeveLength = "short sleeves";
+
+let dressFabric = "cotton";
+let dressFashion = "streight cut";
